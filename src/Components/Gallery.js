f93f159e22c879261ad1660f31acc7bdbef2ca00
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2023-01-18 09:02:24 +0530

    first commit

---
+++ src/Components/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Gallery = () => {
+    return (
+        <div className='Gallery'>
+            <img src='https://loremflickr.com/320/240/brazil,rio' />
+            <img src='https://loremflickr.com/320/240/paris,girl/all' />
+            <img src='https://picsum.photos/200/300' />
+            <img src='https://picsum.photos/seed/picsum/200/300' />
+        </div>
+    )
+}
+
+export default Gallery
---
+++ src/Components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = () => {
+    return (
+        <div className='Heading'>
+            <h1>Random Gallery Picture  !</h1>
+        </div>
+    )
+}
+
+export default Heading
